8f888747594ce971b8ba312e2c2bcb39079a1d9c
Willian <127777836+wlln148@users.noreply.github.com>
2023-09-26 15:56:53 -0300

    Tela da Calculadora em SwiftUI
    Tela do protótipo desenvolvido durante o curso do Hackatruck, realiza calculos de Juros Compostos por Anos ou Meses.

---
+++ Teste/Teste/TesteApp.swift
@@ -0,0 +1,17 @@
+//
+//  TesteApp.swift
+//  Teste
+//
+//  Created by Student on 26/09/23.
+//
+
+import SwiftUI
+
+@main
+struct TesteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
